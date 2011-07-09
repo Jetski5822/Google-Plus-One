b4bbdc6ad4792639ba06a5030dc421a19de65f8a
Nicholas Mayne <Jetski5822@hotmail.com>
2011-07-09 12:10:54 +0100

    added in other script stuff

--- Drivers/GooglePlusonePartDriver.cs
+++ Drivers/GooglePlusonePartDriver.cs
@@ -1,19 +1,11 @@
-﻿using System.Globalization;
-using NGM.GooglePlusone.Models;
-using Orchard;
+﻿using NGM.GooglePlusone.Models;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
 
 namespace NGM.GooglePlusone.Drivers {
     public class GooglePlusonePartDriver : ContentPartDriver<GooglePlusonePart> {
-        private readonly IWorkContextAccessor _workContextAccessor;
-
-        public GooglePlusonePartDriver(IWorkContextAccessor workContextAccessor) {
-            _workContextAccessor = workContextAccessor;
-        }
-
         protected override DriverResult Display(GooglePlusonePart part, string displayType, dynamic shapeHelper) {
-            return ContentShape("Parts_Google_Plusone", () => shapeHelper.Parts_Google_Plusone(ContentPart: part, Culture: GetCulture()));
+            return ContentShape("Parts_Google_Plusone", () => shapeHelper.Parts_Google_Plusone(ContentPart: part));
         }
 
         protected override DriverResult Editor(GooglePlusonePart part, dynamic shapeHelper) {
@@ -24,17 +16,6 @@ namespace NGM.GooglePlusone.Drivers {
         protected override DriverResult Editor(GooglePlusonePart part, IUpdateModel updater, dynamic shapeHelper) {
             updater.TryUpdateModel(part, Prefix, null, null);
             return Editor(part, shapeHelper);
-        }
-
-        private CultureInfo GetCulture() {
-            string currentCultureName = _workContextAccessor.GetContext().CurrentSite.SiteCulture;
-
-            try {
-                return new CultureInfo(currentCultureName);
-            }
-            catch (CultureNotFoundException) {}
-
-            return CultureInfo.CurrentUICulture;
         }
     }
 }
